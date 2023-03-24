65ddba380380d8d4c2f1262315e9d59989a346b8
Veronica isla <veroislagleaniz@gmail.com>
2023-03-24 15:56:24 +0100

    chore: added new folders

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import '../styles/components/App.scss';
+import ExampleComponent from './exampleComponent';
+import React, { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Collections Men Women About Contact Sneaker Company Fall Limited Edition
+        Sneakers These low-profile sneakers are your perfect casual wear
+        companion. Featuring a durable rubber outer sole, they’ll withstand
+        everything the weather can offer. $125.00 50% $250.00 0 Add to cart
+        Challenge by Frontend Mentor. Coded by Your Name Here.
+      </h1>
+      <ExampleComponent />
+    </div>
+  );
+}
+
+export default App;
